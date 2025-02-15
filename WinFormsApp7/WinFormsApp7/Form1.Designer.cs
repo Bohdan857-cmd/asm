61847fe0fc3ad0b1ba5e6eaad3623eaf8d87019c
Bohdan857-cmd <lavrovbogdan2@gmail.com>
2025-02-15 18:04:18 +0100

    Add files via upload

---
+++ WinFormsApp7/WinFormsApp7/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+
+namespace WinFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            textBox1 = new TextBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(344, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(311, 157);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(240, 144);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(270, 307);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += buttonNext_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(528, 307);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private TextBox textBox1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ WinFormsApp7/WinFormsApp7/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WinFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        private string imageFile = @"C:\Users\lavro\OneDrive\e (2)\images.jpg";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowImage();
+        }
+
+        private void ShowImage()
+        {
+            if (File.Exists(imageFile))
+            {
+                pictureBox1.Image = Image.FromFile(imageFile);
+            }
+            else
+            {
+                MessageBox.Show("Зображення не знайдено.");
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            ShowImage();
+        }
+
+        private void buttonPrev_Click(object sender, EventArgs e)
+        {
+            ShowImage();
+        }
+    }
+}
